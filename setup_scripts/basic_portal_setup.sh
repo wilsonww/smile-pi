92359897fa415d2b8009df1370daec3259e2f36c
Reuben Thiessen <reubenthiessen@gmail.com>
2021-08-23 17:07:19 +0000

    Updates portal setup

--- setup_scripts/basic_portal_setup.sh
+++ setup_scripts/basic_portal_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "basic portal setup"
 
-sudo cp ~/smile-pi/setup_files/basic_portal/*.* /usr/share/nginx/html
+sudo cp -r ~/smile-pi/setup_files/basic_portal/*.* /usr/share/nginx/html
 
 cd; cd -
 cd ~/smile-pi
