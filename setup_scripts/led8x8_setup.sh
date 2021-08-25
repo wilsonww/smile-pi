3fc612e2a5b894bf3abda71ed14055dc3896266d
Reuben Thiessen <reubenthiessen@gmail.com>
2021-08-25 02:14:25 +0000

    updates script for led

--- setup_scripts/led8x8_setup.sh
+++ setup_scripts/led8x8_setup.sh
@@ -10,8 +10,9 @@ sudo cp /home/pi/smile-pi/setup_files/smile8x8.service /etc/systemd/system/smile
 sudo systemctl enable smile8x8
 sudo systemctl start smile8x8
 
-echo "sad face when shutting down"
-sudo mkdir /usr/lib/systemd/system-shutdown/
-sudo cp /home/pi/smile-pi/setup_files/shutdownsad8x8.service /etc/systemd/system/shutdownsad8x8.service
+## Note: We used to have two services, one to start and one to stop, but now it's handled all by the smile8x8 service.
+#echo "sad face when shutting down"
+#sudo mkdir /usr/lib/systemd/system-shutdown/
+#sudo cp /home/pi/smile-pi/setup_files/shutdownsad8x8.service /etc/systemd/system/shutdownsad8x8.service
 
 echo "done"
